f4ba26d041461e855b835462672b5eb330cdd290
YOUNESS CHERIFI <formationqlik@gmail.com>
2026-02-07 21:35:19 +0000

    fix: ci error

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,6 +1,6 @@
 select
     order_id,
-    user_name ,
+    user_name as user_id,
     order_status,
     DATETIME(order_date, "Europe/Paris") AS order_created_at,
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
