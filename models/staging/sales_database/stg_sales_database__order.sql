d6b0687ea5cd11e7c76d109661cc1e097533a218
Youness CHERIFI <youness.cherifi@alten.com>
2026-02-08 09:37:01 +0100

    suffix: add suffix column _y

--- models/staging/sales_database/stg_sales_database__order.sql
+++ models/staging/sales_database/stg_sales_database__order.sql
@@ -1,6 +1,6 @@
 select
     order_id,
-    user_name as user_id,
+    user_name as user_id_y,
     order_status,
     DATETIME(order_date, "Europe/Paris") AS order_created_at,
     DATETIME(order_approved_date, "Europe/Paris") AS order_approved_at,
