9ef79decd408fef8eec319cbd9fd1d59e3f709ab
YOUNESS CHERIFI <formationqlik@gmail.com>
2026-02-08 08:28:10 +0000

    delete: delete column item_quantity

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -5,6 +5,6 @@ select CONCAT(order_id, '_', product_id) AS order_item_id,
  DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
  price as unit_price,
  shipping_cost,
- quantity as item_quantity,
+
  (price * quantity) + shipping_cost as total_order_item_amount
 from {{ source('sales_database', 'order_item') }}
